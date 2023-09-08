75b5b604e5a3ef2b6181e611b6f2c37c01f1d0c8
yprice93 <yprice93@github.com>
2023-09-07 20:18:28 -0500

    added message showing user choice and correct answer at the end

--- Documents/python/quiz/main.py
+++ Documents/python/quiz/main.py
@@ -10,8 +10,11 @@ for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(index +1 , "-", alternative)
-    question['user_choise'] = int(input("Enter your answer: "))
-    if question['user_choise'] == question["correct_answer"]:
+    question['user_choice'] = int(input("Enter your answer: "))
+    if question['user_choice'] == question["correct_answer"]:
         score += 1
+
+for index, question in enumerate(data):
+    print(f"You chose for question {index+1} was {question['user_choice']}, but the correct answer was {question['correct_answer']}.")
 
 print(f"Your score is: {score} / {len(data)}")
